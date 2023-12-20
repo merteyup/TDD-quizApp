fb95b7c23de400f6e972dfd9d41e22505b18ad30
merteyup <eupmrt@gmail.com>
2023-12-20 20:15:59 +0300

    Guarantee ResultViewController renders summary on ViewDidLoad.

--- QuizAppTests/ResultViewControllerTest.swift
+++ QuizAppTests/ResultViewControllerTest.swift
@@ -5,4 +5,16 @@
 //  Created by Eyüp Mert on 19.12.2023.
 //
 
-import Foundation
+import XCTest
+@testable import QuizApp
+
+class ResultViewControllerTest: XCTestCase {
+    
+    func test_viewDidLoad_renderSummary() {
+        let sut = ResultViewController(summary: "a Summary", answers: [])
+        
+        _ = sut.view
+        
+        XCTAssertEqual(sut.headerLabel.text, "a Summary")
+    }
+}
