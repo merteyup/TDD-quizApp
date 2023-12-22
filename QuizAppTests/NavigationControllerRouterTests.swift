543583dee12288461652eb904a9989836d73c483
merteyup <eupmrt@gmail.com>
2023-12-22 13:35:32 +0300

    Guarantee that factory uses correct viewControllers and correct amount of them.

--- QuizAppTests/NavigationControllerRouterTests.swift
+++ QuizAppTests/NavigationControllerRouterTests.swift
@@ -12,23 +12,50 @@ import QuizEngine
 
 class NavigationControllerRouterTests: XCTestCase {
     
-    func test_routeToQuestion_presentsQuestionController() {
+    func test_routesToQuestion_presentsQuestionController() {
         let navigationController = UINavigationController()
-        let sut = NavigationControllerRouter(navigationController)
+        let factory = ViewControllerFactoryStub()
+        let viewController = UIViewController()
+        factory.stub(question: "Q1", with: viewController)
+        
+        let sut = NavigationControllerRouter(navigationController, factory: factory)
         
         sut.routeTo(question: "Q1", answerCallback: { _ in })
-        
+
         XCTAssertEqual(navigationController.viewControllers.count, 1)
+        XCTAssertEqual(navigationController.viewControllers.first, viewController)
     }
     
-    func test_routeToQuestionTwice_routesToAnotherQuestion() {
+    func test_routesToSecondQuestion_presentsQuestionController() {
         let navigationController = UINavigationController()
-        let sut = NavigationControllerRouter(navigationController)
-        
+        let factory = ViewControllerFactoryStub()
+        let viewController = UIViewController()
+        let secondViewController = UIViewController()
+        factory.stub(question: "Q1", with: viewController)
+        factory.stub(question: "Q2", with: secondViewController)
+
+        let sut = NavigationControllerRouter(navigationController, factory: factory)
+
         sut.routeTo(question: "Q1", answerCallback: { _ in })
         sut.routeTo(question: "Q2", answerCallback: { _ in })
-
+        
         XCTAssertEqual(navigationController.viewControllers.count, 2)
+        XCTAssertEqual(navigationController.viewControllers.first, viewController)
+        XCTAssertEqual(navigationController.viewControllers.last, secondViewController)
+    }
+    
+    class ViewControllerFactoryStub: ViewControllerFactory {
+        
+        private var stubbedQuestions = [String: UIViewController]()
+        
+        func stub(question: String, with viewController: UIViewController) {
+            stubbedQuestions[question] = viewController
+        }
+        
+        func questionViewController(for question: String, answerCallback: (String) -> Void) -> UIViewController {
+            return stubbedQuestions[question]!
+        }
+        
     }
     
 }
